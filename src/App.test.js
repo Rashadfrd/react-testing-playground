6b049155ae21e6b28c2beb36db48f467161f4c9b
Rashadfrd <rashadnf@code.edu.az>
2023-08-06 02:11:56 +0400

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('should render App component without crashing', () => {
+  render(<App />)
+
+  const element = screen.getByText(/modern frontend testing/i)
+
+  expect(element).toBeInTheDocument();
+})
+

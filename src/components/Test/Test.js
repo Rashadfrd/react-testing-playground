43a899a3e0b0b532a142b9b0722726e5fc286580
Rashadfrd <rashadnf@code.edu.az>
2023-08-08 00:42:54 +0400

    learning about queries

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -8,6 +8,12 @@ function Test() {
 
       <label htmlFor='username'>Username</label>
       <input id='username' type="text" placeholder='Username here...' value="Akif" />
+
+      <img alt="img" src="/incredibles-2.png" />
+
+      <button title='delete'></button>
+
+      <div data-testid="custom-element" />
     </>
   )
 }
--- src/components/Test/Test.test.js
+++ src/components/Test/Test.test.js
@@ -33,6 +33,7 @@ describe("Queries", () => {
 
     expect(element).toBeInTheDocument();
   });
+  
   it("should render an element with the placeholder text Username here...", () => {
     render(<Test />);
 
@@ -40,6 +41,7 @@ describe("Queries", () => {
 
     expect(element).toBeInTheDocument();
   });
+
   it("should render an element with the text First btn", () => {
     render(<Test />);
 
@@ -47,10 +49,37 @@ describe("Queries", () => {
 
     expect(element).toBeInTheDocument();
   });
+
   it("should render an element with the value of Akif", () => {
     render(<Test />);
 
     const element = screen.getByDisplayValue("Akif");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("should render an element with the altText of img", () => {
+    render(<Test />);
+
+    const element = screen.getByAltText("img");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("should render an element with the title of delete", () => {
+    render(<Test />);
+
+    const element = screen.getByTitle("delete");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  //   This is only recommended for cases where you cant match by role or text or it doesnt make sense (e.g. the text is dynamic).
+
+  it("should render an element with the testId of custom-element", () => {
+    render(<Test />);
+
+    const element = screen.getByTestId("custom-element");
 
     expect(element).toBeInTheDocument();
   });

667678c4d0087c977a2bff12ccda4b5c4de2d200
Rashadfrd <rashadnf@code.edu.az>
2023-08-11 23:33:10 +0400

    clipboard and select/deselect events

--- src/components/UserActionsComponent/UserActions.js
+++ src/components/UserActionsComponent/UserActions.js
@@ -9,10 +9,22 @@ function UserActions() {
     });
   };
 
+  const copyHandler = () => {
+    window.navigator.clipboard.writeText("Some text copied here...");
+  };
+
   return (
     <>
       <h3>{count}</h3>
       <button onClick={countHandler}>Increment</button>
+      <button onClick={copyHandler}>Copy</button>
+      <input type="text" />
+
+      <select multiple>
+        <option value="tiger">Tiger</option>
+        <option value="cat">Cat</option>
+        <option value="giraffe">Giraffe</option>
+      </select>
     </>
   );
 }
--- src/components/UserActionsComponent/UserActions.test.js
+++ src/components/UserActionsComponent/UserActions.test.js
@@ -20,7 +20,7 @@ describe("User Actions", () => {
     expect(element).toHaveTextContent(1);
   });
 
-  //Keyboard events
+  // Keyboard events
   it("should render the element correctly", async () => {
     const user = userEvent.setup();
 
@@ -31,5 +31,41 @@ describe("User Actions", () => {
     const element = screen.getByRole("heading");
 
     expect(element).toHaveTextContent(1);
+  });
+
+  // CLipboard events
+  it("should render the element correctly", async () => {
+    const user = userEvent.setup();
+
+    render(<UserActions />);
+
+    await user.keyboard("{Tab}{Tab}{Enter}");
+
+    const element = screen.getByRole("textbox");
+    element.focus();
+
+    await user.paste();
+
+    expect(element).toHaveValue("Some text copied here...");
+  });
+
+  // Select/Deselect events
+  it("should render the element correctly", async () => {
+    const user = userEvent.setup();
+
+    render(<UserActions />);
+
+    const animals = screen.getByRole('listbox');
+
+    await userEvent.selectOptions(animals,['giraffe','tiger'])
+
+    expect(screen.getByRole('option', {name: 'Giraffe'}).selected).toBe(true)
+    expect(screen.getByRole('option', {name: 'Cat'}).selected).toBe(false)
+    expect(screen.getByRole('option', {name: 'Tiger'}).selected).toBe(true)
+
+    await userEvent.deselectOptions(animals,['giraffe'])
+
+    expect(screen.getByRole('option', {name: 'Giraffe'}).selected).toBe(false)
+
   });
 });
